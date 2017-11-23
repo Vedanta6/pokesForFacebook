21db4e55d32887677acaf6c12cbf01d5f9760e7b
Nina <vedanta6@gmail.com>
2017-11-23 13:42:57 +0300

    Update connect_selenium.py

--- connect_selenium.py
+++ connect_selenium.py
@@ -1,18 +1,19 @@
-'''
-Required installations:
-    - selenium (pip install selenium)
-    - chromedriver (https://sites.google.com/a/chromium.org/chromedriver/)
-'''
+##
+# Required installations:
+#    - selenium (pip install selenium)
+#    - chromedriver (https://sites.google.com/a/chromium.org/chromedriver/)
+##
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
 import time
 import config
 
 # browser init
 options = Options()
 options.add_argument("--disable-notifications")
-browser = webdriver.Chrome('C:\Python34\Lib\site-packages\selenium\webdriver\chromedriver.exe', chrome_options=options)
+browser = webdriver.Chrome(config.CHROME_DRIVER, chrome_options=options)
 
 browser.get('http://facebook.com')
 time.sleep(2)
@@ -29,12 +30,14 @@ login = browser.find_element_by_id("loginbutton").click()
 # pokes page
 browser.get('http://facebook.com/pokes')
 
-while(1):
+while 1:
     try:
         browser.find_element_by_link_text("Poke back").click()
         # time.sleep(1)
     except (KeyboardInterrupt, SystemExit):
         browser.close()
         break
-    except Exception:
+    except NoSuchElementException:
         pass
+    except WebDriverException:
+        break
